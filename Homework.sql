059b4c890439ac5f44f35f0d502e8546f5596829
Xiyue Deng <linxiaholly@gmail.com>
2018-09-12 20:30:32 -0700

    updated the correct file

--- Homework.sql
+++ Homework.sql
@@ -84,15 +84,76 @@ select first_name, last_name, sum(amount)as 'Total Paid'
 from customer join payment on customer.customer_id = payment.customer_id
 group by first_name, last_name
 order by last_name;
--- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+-- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. 
+-- Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+select title
+from film
+where (title like 'K%' OR title like 'Q%') and language_id = 
+(select language_id from language where name ='English');
+
 -- 7b. Use subqueries to display all actors who appear in the film Alone Trip.
+select first_name, last_name ,upper(concat(first_name,' ',last_name)) as 'Actor Name'
+from actor
+where actor_id  in
+(select actor_id
+from film_actor
+where film_id = 
+(select film_id
+from film 
+where title = "Alone Trip"));
+
 -- 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
+select first_name, last_name, email, country
+from customer join address on customer.address_id = address. address_id
+join city on city.city_id = address.city_id
+join country on country.country_id = city.country_id
+where country = "Canada";
+
 -- 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
+select title
+from film inner join film_category on film_category.film_id = film.film_id
+inner join category on film_category.category_id = category.category_id
+where name = "family";
+
 -- 7e. Display the most frequently rented movies in descending order.
+select inventory.film_id, title ,count(film.film_id) as 'Rental Frequency'
+from film join inventory on inventory.film_id = film.film_id
+join rental on rental.inventory_id = inventory.inventory_id
+group by  inventory.film_id, title
+order by count(film.film_id) desc;
+
 -- 7f. Write a query to display how much business, in dollars, each store brought in.
+select store_id, sum(amount)
+from payment join rental on rental.rental_id  =  payment.rental_id
+join inventory on inventory.inventory_id  = rental.inventory_id
+group by store_id;
+
 -- 7g. Write a query to display for each store its store ID, city, and country.
+select store.store_id, city, country
+from store join address on address.address_id = store.address_id
+ join city on city.city_id = address.city_id
+ join country on country.country_id  = city.country_id;
+
 -- 7h. List the top five genres in gross revenue in descending order. (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+select category.name, sum(amount) as'Gross Revenue'
+from category inner join film_category on category.category_id = film_category.category_id
+inner join inventory on inventory.film_id = film_category.film_id
+inner join rental on rental.inventory_id = inventory.inventory_id
+inner join payment on payment.rental_id = rental.rental_id
+group by category.name
+order by sum(amount) desc limit 5;
 
+-- 8a.for update
+create view Top_5_genres_by_gross_revenue as 
+select category.name, sum(amount) as'Gross Revenue'
+from category inner join film_category on category.category_id = film_category.category_id
+inner join inventory on inventory.film_id = film_category.film_id
+inner join rental on rental.inventory_id = inventory.inventory_id
+inner join payment on payment.rental_id = rental.rental_id
+group by category.name
+order by sum(amount) desc limit 5;
 
-
-
+-- 8b.show view
+show create view Top_5_genres_by_gross_revenue;
+-- delect view
+drop view Top_5_genres_by_gross_revenue;
